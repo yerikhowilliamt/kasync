eb318107c6c3841ee16c6138a384dd8e2b36f4d2
Yerikho William <yerikhowilliamt@gmail.com>
2026-08-13 13:42:45 +0700

    fix(dto): replace prisma enum in class-validator IsEnum for AccountType to avoid runtime initialization error

--- src/modules/accounts/dto/create-account.dto.ts
+++ src/modules/accounts/dto/create-account.dto.ts
@@ -2,14 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { AccountType } from '@prisma/client';
 
+export enum AccountTypeEnum {
+  ASSET = 'ASSET',
+  LIABILITY = 'LIABILITY',
+  EQUITY = 'EQUITY',
+  REVENUE = 'REVENUE',
+  EXPENSE = 'EXPENSE',
+}
+
 export class CreateAccountDto {
   @ApiProperty({ description: 'The name of the account' })
   @IsString()
   @IsNotEmpty()
   name!: string;
 
-  @ApiProperty({ enum: AccountType, description: 'The type of the account' })
-  @IsEnum(AccountType)
+  @ApiProperty({ description: 'The type of the account', enum: AccountTypeEnum })
+  @IsEnum(AccountTypeEnum)
   @IsNotEmpty()
   type!: AccountType;
 }
